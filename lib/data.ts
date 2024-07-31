6ebde46876b8544c6f7c0f19f5a9ec4a686f2d07
Sk Sahel <sksahel@Sks-MacBook-Air.local>
2024-07-31 23:22:07 +0600

    Fully Responsive My Portfolio Website

---
+++ lib/data.ts
@@ -0,0 +1,190 @@
+import {
+  BrainCircuit,
+  Code,
+  Facebook,
+  Github,
+  Globe,
+  Instagram,
+  LayoutTemplate,
+  Linkedin,
+  Monitor,
+  MousePointerClick,
+  PaintRoller,
+  PencilLine,
+  Rocket,
+  ShoppingBag,
+  TabletSmartphone,
+  Twitter,
+} from "lucide-react";
+
+export const menuLinks = [
+  {
+    name: "Home",
+    hidden: "Back to home",
+    link: "/",
+  },
+  {
+    name: "Portfolio",
+    hidden: "Amazing things",
+    link: "#projects",
+  },
+  {
+    name: "Services",
+    hidden: "What I can do",
+    link: "#services",
+  },
+  {
+    name: "Contact",
+    hidden: "Get in touch",
+    link: "#testimonial",
+  },
+];
+
+export const services = [
+  {
+    title: "Web Development",
+    description:
+      "I build fast, interactive websites that are easy to navigate and are built with the latest technologies.",
+    icon: Monitor,
+  },
+  {
+    title: "E-commerce",
+    description:
+      "I develop e-commerce websites that are easy to use and have a great user experience.",
+    icon: ShoppingBag,
+  },
+  {
+    title: "Web Hosting",
+    description: "I provide web hosting services that are reliable and secure.",
+    icon: Globe,
+  },
+];
+
+export const projects = [
+  {
+    title: "Online Marketplace Development with Next.js",
+    image: "/portfolio/1.jpg",
+    tags: ["Marketplace"],
+  },
+  {
+    title: "Fashion Ecommerce Platform Built with Next.js",
+    image: "/portfolio/2.jpg",
+    tags: ["Fashion", "Ecommerce"],
+  },
+  {
+    title: "Customizable Ecommerce Solution Using Next.js",
+    image: "/portfolio/3.jpg",
+    tags: ["Ecommerce", "Customizable"],
+  },
+  {
+    title: "Next.js Ecommerce Application for Digital Products",
+    image: "/portfolio/4.jpg",
+    tags: ["Ecommerce", "Digital"],
+  },
+  {
+    title: "Next.js Ecommerce Platform for Artisans",
+    image: "/portfolio/1.jpg",
+    tags: ["Ecommerce", "Artisans"],
+  },
+  {
+    title: "Digital Marketplace Development with Next.js",
+    image: "/portfolio/2.jpg",
+    tags: ["Marketplace", "Digital"],
+  },
+  {
+    title: "Next.js-based Fashion Ecommerce Store",
+    image: "/portfolio/3.jpg",
+    tags: ["Fashion", "Ecommerce"],
+  },
+  {
+    title: "Customizable Ecommerce Solution Using Next.js",
+    image: "/portfolio/4.jpg",
+    tags: ["Ecommerce", "Customizable"],
+  },
+];
+
+export const processSteps = [
+  {
+    title: "Planning",
+    description: "Define goal, wireframe, design concept, content write.",
+    icon: BrainCircuit,
+  },
+  {
+    title: "Design",
+    description: "Create mockups, design UI, and UX.",
+    icon: PaintRoller,
+  },
+  {
+    title: "Development",
+    description: "Develop the website or application.",
+    icon: Code,
+  },
+
+  {
+    title: "Launch",
+    description: "Launch the website or application.",
+    icon: Rocket,
+  },
+];
+
+export const SocialLinksData = [
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/share/5mVfE71osbprvLn2/?mibextid=LQQJ4d",
+    icon: Facebook,
+  },
+  {
+    name: "Github",
+    link: "https://github.com/SheikhSahel",
+    icon: Github,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/the_precious_sk?igsh=MWhxaTJ3dHZmc29iMg%3D%3D&utm_source=qr",
+    icon: Instagram,
+  },
+  {
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/sk-sahel/",
+    icon: Linkedin,
+  },
+];
+
+export const TestimonialsData = [
+  {
+    name: "John Doe",
+    title: "CEO, Company",
+    description:
+      "Sahel is a talented developer who is always willing to go the extra mile to ensure that the project is completed to the highest standard. He is a great communicator and always keeps me updated on the progress of the project. I highly recommend him to anyone looking for a reliable and skilled developer.",
+  },
+  {
+    name: "John Doe",
+    title: "CEO, Company",
+    description:
+      "Sahel is a talented developer who is always willing to go the extra mile to ensure that the project is completed to the highest standard. He is a great communicator and always keeps me updated on the progress of the project. I highly recommend him to anyone looking for a reliable and skilled developer.",
+  },
+  {
+    name: "John Doe",
+    title: "CEO, Company",
+    description:
+      "Sahel is a talented developer who is always willing to go the extra mile to ensure that the project is completed to the highest standard. He is a great communicator and always keeps me updated on the progress of the project. I highly recommend him to anyone looking for a reliable and skilled developer.",
+  },
+  {
+    name: "John Doe",
+    title: "CEO, Company",
+    description:
+      "Sahel is a talented developer who is always willing to go the extra mile to ensure that the project is completed to the highest standard. He is a great communicator and always keeps me updated on the progress of the project. I highly recommend him to anyone looking for a reliable and skilled developer.",
+  },
+  {
+    name: "John Doe",
+    title: "CEO, Company",
+    description:
+      "Sahel is a talented developer who is always willing to go the extra mile to ensure that the project is completed to the highest standard. He is a great communicator and always keeps me updated on the progress of the project. I highly recommend him to anyone looking for a reliable and skilled developer.",
+  },
+  {
+    name: "John Doe",
+    title: "CEO, Company",
+    description:
+      "Sahel is a talented developer who is always willing to go the extra mile to ensure that the project is completed to the highest standard. He is a great communicator and always keeps me updated on the progress of the project. I highly recommend him to anyone looking for a reliable and skilled developer.",
+  },
+];
